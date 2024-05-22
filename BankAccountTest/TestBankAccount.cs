5de24ee0b23bea4a0ace369ef0816607143f75c1
jxjx359 <jonashofacker9@gmail.com>
2024-05-22 17:48:49 +0200

    Projektdateien hinzufügen.

---
+++ BankAccountTest/TestBankAccount.cs
@@ -0,0 +1,14 @@
+﻿using MyApp;
+
+namespace BankAccountTest
+{
+    internal class TestBankAccount
+    {
+        [Fact]
+        public void MakeDeposit_ShouldIncreaseBalance()
+        {
+            // Arrange
+
+        }
+    }
+}
